19f43e9ba7c23dfbb19416b99e8875c63af47fb4
dhzdhd <ragas5432100@gmail.com>
2023-04-22 09:21:22 +0530

    Add router

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
+  import {Router, Link, Route} from 'svelte-navigator';
+  import Login from './lib/routes/Login.svelte';
+  import Home from './lib/routes/Home.svelte';
+
   async function fet() {
     const res = await fetch('http://localhost:3000/api/tables');
     return await res.text();
   }
 </script>
 
-<main>
-  {#await fet()}
-    <p>load</p>
-  {:then data}
-    <p>{data}</p>
-  {/await}
-</main>
+<Router>
+  <nav>
+		<Link to="/">Login</Link>
+		<Link to="home">Home</Link>
+	</nav>
+	<div>
+		<Route path="/">
+			<Login  />
+		</Route>
+		<Route path="home">
+      <Home />
+    </Route>
+	</div>
+</Router>
---
+++ src/lib/routes/Login.svelte
@@ -0,0 +1,8 @@
+<main>
+    <form action="">
+        <label for="">Username</label>
+        <input type="text">
+        <label for="">Password</label>
+        <input type="text">
+    </form>
+</main>
